f062210e1cc2a92cf12702e13a99000267565644
Bryan <brymac@gmail.com>
2015-07-21 16:28:11 -0400

    Projects controller created

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root :to => 'users#index'
   resources :user_sessions
   resources :users
+  resources :projects
   
 
  get 'login' => 'user_sessions#new', :as => :login
