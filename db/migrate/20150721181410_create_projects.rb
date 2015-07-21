e337fa1ec5f04004ea36434f5ee1c939324c1817
Nick Van Exan <nick.vanexan@gmail.com>
2015-07-21 14:23:21 -0400

    Adding Project model

---
+++ db/migrate/20150721181410_create_projects.rb
@@ -0,0 +1,15 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.integer :funding_goal
+      t.datetime :start_date
+      t.datetime :end_date
+      t.integer :owner_id, index: true
+
+
+      t.timestamps null: false
+    end
+  end
+end
