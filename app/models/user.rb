b9e40c5229dcf47f48691910ee47f03e9062bc39
Nick Van Exan <nick.vanexan@gmail.com>
2015-07-23 17:10:21 -0400

    Adding project owner to project show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   validates :email, uniqueness: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
